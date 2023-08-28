fc5e1991c675599e0335a9a56ad909cfb7274bbb
ERNEST CHRIS <ernestchris6@gmail.com>
2023-08-28 10:41:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+import Quiz from './components/quiz'
+import data from './components/data';
+import Timer from './components/Timer';
+
+//Hooks are the new features introduced in the react 18 version, 
+//It allows you to use a state and other react features without writing a class.
+//Hooks are the function which hook into react state and life cycle features from 
+//function components.
+
+function App() {
+
+    const [questionNumber, setQuestionNumber] = useState(1)
+    const [stopTime, setStopTime] = useState(false)
+    const [earned, setEarned] = useState('$ 0')
+    
+    
+    const moneyPyramid = [
+        {id : 1, amount : "$ 100"},
+        {id : 2, amount : "$ 200"},
+        {id : 3, amount : "$ 300"},
+        {id : 4, amount : "$ 400"},
+        {id : 5, amount : "$ 500"},
+        {id : 6, amount : "$ 1000"},
+        {id : 7, amount : "$ 2000"},
+        {id : 8, amount : "$ 3000"},
+        {id : 9, amount : "$ 4000"},
+        {id : 10, amount : "$ 8000"},
+        {id : 11, amount : "$ 16000"},
+        {id : 12, amount : "$ 32000"},
+        {id : 13, amount : "$ 64000"},
+        {id : 14, amount : "$ 125000"},
+        {id : 15, amount : "$ 250000"},
+        {id : 16, amount : "$ 500000"},
+        {id : 17, amount : "$1000000"},
+    ].reverse();
+
+    useEffect(() => {
+      questionNumber > 1 && setEarned(moneyPyramid.find(m => m.id === questionNumber -1).amount)
+        }, [moneyPyramid, questionNumber])
+    
+
+    return(
+        <div className='app'>
+            <div className='main'>
+                {stopTime ? <h1 className='endText'> Congratulations You Won : {earned}</h1> : (
+                    <>
+                        <div className='top'>
+                        <div className='timer'>
+                        <Timer setStopTime={setStopTime} questionNumber={questionNumber}/>
+                        </div>
+                    </div>
+                    <div className='bottom'>
+                        <Quiz 
+                            data={data} 
+                            setStopTime={setStopTime} 
+                            questionNumber={questionNumber} 
+                            setQuestionNumber={setQuestionNumber} />
+                    </div>
+                </>
+                )}
+                
+            </div>
+            <div className='pyramid'>
+                <ul className='moneyList'>
+                    {moneyPyramid.map(m=>
+                    <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+                        <span className='moneyListItemNumber'>{m.id}</span>
+                        <span className='moneyListAmount'>{m.amount}</span>
+                    </li>)}
+                    
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/data.js
@@ -0,0 +1,484 @@
+const data = [
+    {
+            id : 1,
+            question : "Rolex is a company that specializes in what type of product?",
+            answer : [
+            {
+                text : "Phone",
+                correct : false
+            },
+
+            {
+                text : "Food",
+                correct : false
+            },
+
+            {
+                text : "Watches",
+                correct : true
+            },
+
+            {
+                text : "Cosmetics",
+                correct : false
+            },
+
+
+        ]
+
+    },
+
+    {
+        id : 2,
+        question : "Who is the first black president of America?",
+        answer : [
+        {
+            text : "Goodluck Jonathan",
+            correct : false
+        },
+
+        {
+            text : "Barack Obama",
+            correct : true
+        },
+
+        {
+            text : "Fidel Castro",
+            correct : false
+        },
+
+        {
+            text : "Jacob Zuma",
+            correct : false
+        },
+
+
+    ]
+
+},
+
+{
+    id : 3,
+    question : "When was the website Facebook lauched?",
+    answer : [
+    {
+        text : "2002",
+        correct : false
+    },
+
+    {
+        text : "2008",
+        correct : false
+    },
+
+    {
+        text : "2010",
+        correct : false
+    },
+
+    {
+        text : "2004",
+        correct : true
+    },
+
+
+]
+
+},
+
+{
+    id : 4,
+    question : "The mind is a blank state 'Tabula Rasa' was attributed to?",
+    answer : [
+    {
+        text : "Socrates",
+        correct : false
+    },
+
+    {
+        text : "Aristotle",
+        correct : false
+    },
+
+    {
+        text : "John Locke",
+        correct : true
+    },
+
+    {
+        text : "Renes Descartes",
+        correct : false
+    },
+
+
+]
+
+},
+
+{
+    id : 5,
+    question : "Who played the character Harry Potter in the movie?",
+    answer : [
+    {
+        text : "Denzel Washington",
+        correct : true
+    },
+
+    {
+        text : "Leonardo Di Caprio",
+        correct : false
+    },
+
+    {
+        text : "Daniel Redcliff",
+        correct : true
+    },
+
+    {
+        text : "Vin Diesel",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 6,
+    question : "What sort of animal is Walt Disney's Dumbo?",
+    answer : [
+    {
+        text : "Deer",
+        correct : false
+    },
+
+    {
+        text : "Elephant",
+        correct : true
+    },
+
+    {
+        text : "Rabbit",
+        correct : true
+    },
+
+    {
+        text : "Donkey",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 7,
+    question : "Queen Anne was the daughter of which English Monarch?",
+    answer : [
+    {
+        text : "James III",
+        correct : true
+    },
+
+    {
+        text : "Henry VIII",
+        correct : false
+    },
+
+    {
+        text : "Queen Elizabeth",
+        correct : false
+    },
+
+    {
+        text : "William I",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 8,
+    question : "What is the equivalent of 77 degrees Fahrenheit?",
+    answer : [
+    {
+        text : "15",
+        correct : false
+    },
+
+    {
+        text : "20",
+        correct : false
+    },
+
+    {
+        text : "25",
+        correct : true
+    },
+
+    {
+        text : "30",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 9,
+    question : "Which is the largest city in the USA's largest states?",
+    answer : [
+    {
+        text : "Dallas",
+        correct : false
+    },
+
+    {
+        text : "Anchorage",
+        correct : true
+    },
+
+    {
+        text : "New York",
+        correct : false
+    },
+
+    {
+        text : "Los Angeles",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 10,
+    question : "The word 'ARISTOCRACY' literally means power in the hands of?",
+    answer : [
+    {
+        text : "The few",
+        correct : false
+    },
+
+    {
+        text : "The rich",
+        correct : false
+    },
+
+    {
+        text : "The barons",
+        correct : false
+    },
+
+    {
+        text : "The best",
+        correct : true
+    },
+
+ ]
+
+},
+
+{
+    id : 11,
+    question : "In which palace was Queen Elizabeth I born?",
+    answer : [
+    {
+        text : "Greenwich",
+        correct : true
+    },
+
+    {
+        text : "Richmond",
+        correct : false
+    },
+
+    {
+        text : "Hampton court",
+        correct : false
+    },
+
+    {
+        text : "Kesington",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 12,
+    question : "Who named Nigeria?",
+    answer : [
+    {
+        text : "Mary Slessor",
+        correct : false
+    },
+
+    {
+        text : "Lord Lugard",
+        correct : false
+    },
+
+    {
+        text : "Flora Shaw",
+        correct : true
+    },
+
+    {
+        text : "Magaret Thatcher",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 13,
+    question : "The ancient city of Carthage is present day?",
+    answer : [
+    {
+        text : "Tunisia",
+        correct : true
+    },
+
+    {
+        text : "Algeria",
+        correct : false
+    },
+
+    {
+        text : "Morocco",
+        correct : true
+    },
+
+    {
+        text : "Sudan",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 14,
+    question : "Where would a peruke be worn?",
+    answer : [
+    {
+        text : "On the wrist",
+        correct : false
+    },
+
+    {
+        text : "On the head",
+        correct : true
+    },
+
+    {
+        text : "Around the waist",
+        correct : false
+    },
+
+    {
+        text : "Around the neck",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 15,
+    question : "The origin of clothing began with?",
+    answer : [
+    {
+        text : "The Modern man",
+        correct : false
+    },
+
+    {
+        text : "The Cro-magnon man",
+        correct : false
+    },
+
+    {
+        text : "The Neanderthal man",
+        correct : true
+    },
+
+    {
+        text : "The Mad man",
+        correct : false
+    },
+
+ ]
+
+},
+
+{
+    id : 16,
+    question : "Who proposed the evolution theory?",
+    answer : [
+    {
+        text : "Gregor Mendel",
+        correct : false
+    },
+
+    {
+        text : "Ernest Rutherford",
+        correct : false
+    },
+
+    {
+        text : "Albert Einstein",
+        correct : false
+    },
+
+    {
+        text : "Charles Darwin",
+        correct : true
+    },
+
+ ]
+
+},
+
+{
+    id : 17,
+    question : "Which is the first university in Nigeria and in what year was it founded?",
+    answer : [
+    {
+        text : "University of Lagos 1962",
+        correct : true
+    },
+
+    {
+        text : "University of Nigeria 1960",
+        correct : false
+    },
+
+    {
+        text : "University of Ibadan 1948",
+        correct : true
+    },
+
+    {
+        text : "University of Benin 1970",
+        correct : false
+    },
+
+ ]
+
+}
+ 
+]
+
+export default data;
