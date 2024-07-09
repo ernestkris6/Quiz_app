9adefe2a2e0937d7a359dfac0596379a6d0ac0b0
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-09 18:07:07 +0100

    isLoading

--- src/Converter.js
+++ src/Converter.js
@@ -16,16 +16,21 @@ function Converter() {
     const [fromCur, setFromCurr] = useState("EUR");
     const [toCurr, setToCurr] = useState("USD");
     const [output, setOutput] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(function(){
         async function getConversion() {
 
+            setIsLoading(true)
             const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCur}&to=${toCurr}`)
     
             const data = await res.json();
             setOutput(data.rates[toCurr])
+            //console.log(data);
+            setIsLoading(false)
         }
 
+        if(fromCur === toCurr) return setAmount(amount);
         
         getConversion();
     }, [amount, fromCur, toCurr])
@@ -37,22 +42,29 @@ function Converter() {
         placeholder='Enter amount'
         value={amount}
         onChange={(e)=> setAmount(Number(e.target.value))}
+        disabled={isLoading}
         />
-        <select value={fromCur} onChange={(e)=> setFromCurr(e.target.value)}>
-            <option>USD</option>
-            <option>EUR</option>
-            <option>CAN</option>
-            <option>INR</option>
+        <select 
+        value={fromCur} 
+        onChange={(e)=> setFromCurr(e.target.value)}
+        disabled={isLoading}>
+            <option value={"USD"}>USD</option>
+            <option value={"EUR"}>EUR</option>
+            <option value={"CAD"}>CAD</option>
+            <option value={"INR"}>INR</option>
+            <option value={"GBP"}>GBP</option>
         </select>
 
-        <select value={toCurr} onChange={(e)=> setToCurr(e.target.value)}>
-            <option>EUR</option>
-            <option>USD</option>
-            <option>INR</option>
-            <option>CAN</option>
+        <select value={toCurr} onChange={(e)=> setToCurr(e.target.value)}
+        disabled={isLoading}>
+            <option value={"EUR"}>EUR</option>
+            <option value={"USD"}>USD</option>
+            <option value={"INR"}>INR</option>
+            <option value={"CAD"}>CAD</option>
+            <option value={"GBP"}>GBP</option>
         </select>
 
-        <p style={amountStyle}>{output}</p>
+        <p style={amountStyle}>{output} {toCurr}</p>
     </div>
   )
 }
