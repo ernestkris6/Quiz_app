eb0c3a2de1768827649c4e557330f370a14c1419
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-09 16:51:15 +0100

    states

--- src/Converter.js
+++ src/Converter.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Converter = () => {
+
+const style= {
+    margin : "12px"
+}
+
+const amountStyle = {
+    fontSize : "12px",
+    marginTop : "12px"
+}
+
+function Converter() {
+
+    const [amount, setAmount] = useState(1);
+    const [fromCur, setFromCurr] = useState("USD");
+    const [toCurr, setToCurr] = useState("EUR");
+    // const [] = useState("");
+
   return (
-    <div>Converter</div>
+    <div style={style}>
+        <input 
+        type='text' 
+        placeholder='Enter amount'
+        value={amount}
+        onChange={(e)=> setAmount(e.target.value)}
+        />
+        <select>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>CAN</option>
+        </select>
+
+        <select>
+            <option>EUR</option>
+            <option>USD</option>
+            <option>CAN</option>
+        </select>
+
+        <p style={amountStyle}>AMOUNT</p>
+    </div>
   )
 }
 
